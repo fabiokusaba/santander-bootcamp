73d7cb86e4ad48a6c188bcd965492ac4c3a9eba2
fabiokusaba <fabinhokb44@gmail.com>
2024-05-11 19:38:02 -0300

    add: UserRepository

---
+++ src/main/java/me/dio/santanderbootcamp/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.dio.santanderbootcamp.domain.repository;
+
+import me.dio.santanderbootcamp.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
